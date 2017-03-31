76981145dd1ebff47b86e4a682e47dd1926598a0
lee2 <leepang15@gmail.com>
2017-03-31 13:15:06 -0700

    first commit

---
+++ RPN_calculator.py
@@ -0,0 +1,74 @@
+
+# coding: utf-8
+
+# In[6]:
+
+class RpnCalc():
+    
+    def __init__(self):
+        self.stack = []
+        
+    def add_to_stack(self, inpt):
+        self.stack.append(inpt)
+
+    def swap(self):
+        a, b = self.remove_from_stack()
+        self.add_to_stack(b)
+        self.add_to_stack(a)
+
+    def remove_from_stack(self):
+        b = self.stack.pop()
+        a = self.stack.pop()
+        return a, b
+        
+    def operation(self, inpt):
+        a, b = self.remove_from_stack()
+        if inpt == '+':
+            result = a + b
+        elif inpt == '-':
+            result = a - b
+        elif inpt == '*':
+            result = a * b
+        elif inpt == '/':
+            result = a / b
+        self.add_to_stack(result)
+        
+    def run(self):
+        inpt = raw_input(': ')
+        while inpt != 'q':
+            if inpt in ['+', '-', '*', '/']:
+                self.operation(inpt)
+            elif inpt == 'c':
+                self.__init__()
+            elif inpt == 'swap':
+                self.swap()
+            else:
+                self.add_to_stack(float(inpt))
+            print self.stack
+            inpt = raw_input(': ')
+
+
+# In[7]:
+
+calc = RpnCalc()
+
+
+# In[8]:
+
+calc.run()
+
+
+# In[ ]:
+
+
+
+
+# In[5]:
+
+int(0.5)
+
+
+# In[ ]:
+
+
+
